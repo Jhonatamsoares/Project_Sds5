b16dafbefc74924122ad119cf6f9292e456c0630
Jhonatamsoares <jhonatamtecnologias@gmail.com>
2021-11-05 00:09:29 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/Semana/devsuperior/SemanaDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Semana.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaDevsuperiorApplication.class, args);
+	}
+
+}
