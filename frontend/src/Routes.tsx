e9f38641708b54cc63477455cc6b81f682845d00
Jhonatamsoares <jhonatamtecnologias@gmail.com>
2021-11-10 22:05:28 -0300

    Reparo Home

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,15 +1,20 @@
 import Dashboard from "Pages/Dashboard";
+import Home from "Pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function Routes() {
     return (
-       <BrowserRouter>
-             <Switch>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
                 <Route path="/dashboard">
                     <Dashboard />
-                </Route>
-             </Switch>
-       </BrowserRouter>
+                    <Home />
+            </Route>
+        </Switch>
+       </BrowserRouter >
 
     );
 }
